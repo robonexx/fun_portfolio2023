39efddc0ed0d39bb662d2019349bbfbc7ae6abf9
robonexx <robertwagar@gmail.com>
2023-10-08 17:03:52 +0200

    fundamentals / tech /skills card

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,4 +1,4 @@
-import './Skills.scss';
+import styles from './skills.module.scss';
 import Gta1 from '../../assets/images/gtaimage/gta1.jpg';
 import Gta2 from '../../assets/images/gtaimage/gta2.jpg';
 import Gta3 from '../../assets/images/gtaimage/gta3.jpg';
@@ -11,76 +11,44 @@ import Css from '../../assets/images/techimages/css.png';
 import ReactImg from '../../assets/images/techimages/react.png';
 import Node from '../../assets/images/techimages/node.png';
 import Github from '../../assets/images/techimages/github.png';
+import SkillCard from './SkillsCard';
 
-const skillCard = () => {
+const data = [
+  {
+    imageFront: Gta1,
+    imageBack: Html,
+  },
+  {
+    imageFront: Gta2,
+    imageBack: Css,
+  },
+  {
+    imageFront: Gta3,
+    imageBack: Js,
+  },
+  {
+    imageFront: Gta4,
+    imageBack: ReactImg,
+  },
+  {
+    imageFront: Gta5,
+    imageBack: Node,
+  },
+  {
+    imageFront: Gta6,
+    imageBack: Github,
+  },
+];
 
-}
-
- const Skills = () => {
+const Skills = () => {
   return (
-    <div className='card-container'>
-      <div className='card'>
-        <div className='card-inner'>
-          <div className='card-front'>
-            <img src={Gta1} alt='' />
-          </div>
-          <div className='card-back'>
-            <img src={Js} alt='' />
-          </div>
-        </div>
-      </div>
-      <div className='card'>
-        <div className='card-inner'>
-          <div className='card-front'>
-            <img src={Gta2} alt='' />
-          </div>
-          <div className='card-back'>
-            <img src={Html} alt='' />
-          </div>
-        </div>
-      </div>
-      <div className='card'>
-        <div className='card-inner'>
-          <div className='card-front'>
-            <img src={Gta3} alt='' />
-          </div>
-          <div className='card-back'>
-            <img src={Css} alt='' />
-          </div>
-        </div>
-      </div>
-      <div className='card'>
-        <div className='card-inner'>
-          <div className='card-front'>
-            <img src={Gta4} alt='' />
-          </div>
-          <div className='card-back'>
-            <img src={ReactImg} alt='' />
-          </div>
-        </div>
-      </div>
-      <div className='card'>
-        <div className='card-inner'>
-          <div className='card-front'>
-            <img src={Gta5} alt='' />
-          </div>
-          <div className='card-back'>
-            <img src={Node} alt='' />
-          </div>
-        </div>
-      </div>
-      <div className='card'>
-        <div className='card-inner'>
-          <div className='card-front'>
-            <img src={Gta6} alt='' />
-          </div>
-          <div className='card-back'>
-            <img src={Github} alt='' />
-          </div>
-        </div>
-      </div>
+    <div className={styles.card_container}>
+      {data.map((c, i) => (
+        // eslint-disable-next-line react/jsx-key
+        <SkillCard {...c} key={i} />
+      ))}
     </div>
   );
-}
+};
 
-export default Skills
+export default Skills;
