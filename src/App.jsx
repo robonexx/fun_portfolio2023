9568ba79039b499706d1e59787e53fd91fffcad8
robonexx <robertwagar@gmail.com>
2023-10-08 10:44:06 +0200

    working on styling, setting base font sizes, fonts, etc

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1>New portfolio- lets go</h1>
-        
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam maiores nemo exercitationem, dicta voluptas commodi quam dolores eos amet ea quaerat provident delectus impedit? Quia quam enim modi hic? Excepturi.</p>
     </>
   )
 }
