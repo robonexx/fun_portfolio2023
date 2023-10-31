6711d0c2e129074cf69045bd78b19dcaff8dfe70
robonexx <robertwagar@gmail.com>
2023-10-31 13:53:11 +0100

    tried using chat gpt but it broke my code... lol

--- src/components/typewriter/TypeWriter.jsx
+++ src/components/typewriter/TypeWriter.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './typewriter.scss';
 
 const words = [
@@ -7,55 +7,58 @@ const words = [
   'Terve',
   'Hola',
   'Hi... I`m Rob',
-  'A Frontend developer based in Stocholm, Sweden',
+  'A Frontend developer based in Stockholm, Sweden',
   'I LIKE CREATING SMART USER INTERFACES & USEFUL INTERACTIONS',
   'RESPONSIVE AND "UP TO DATE" WEBSITES',
   'DEVELOPING RICH WEB EXPERIENCES & APPLICATIONS',
 ];
 
-export default function TypeWriter() {
+const TypeWriter = () => {
   const [index, setIndex] = useState(0);
   const [subIndex, setSubIndex] = useState(0);
   const [blink, setBlink] = useState(true);
-  const [reverse, setReverse] = useState(false);
 
   // typeWriter
   useEffect(() => {
     if (index === words.length) return;
 
-    if (
-      subIndex === words[index].length + 1 &&
-      index !== words.length - 1 &&
-      !reverse
-    ) {
-      setReverse(true);
-      return;
+    if (subIndex === words[index].length + 1) {
+      // If the word is fully typed
+      if (index === words.length - 1) {
+        // If it's the last word, wait for a delay and then reset to the first word
+        setTimeout(() => {
+          setIndex(0);
+          setSubIndex(0);
+        }, 1000); // You can adjust the delay before resetting
+      } else {
+        // Move to the next word
+        setTimeout(() => {
+          setIndex((prev) => prev + 1);
+          setSubIndex(0);
+        }, 1000); // You can adjust the delay before moving to the next word
+      }
+    } else {
+      // Continue typing the current word
+      const timeout = setTimeout(() => {
+        setSubIndex((prev) => prev + 1);
+      }, 60); // You can adjust the typing speed
+      return () => clearTimeout(timeout);
     }
-
-    if (subIndex === 0 && reverse) {
-      setReverse(false);
-      setIndex((prev) => prev + 1);
-      return;
-    }
-
-    const timeout = setTimeout(() => {
-      setSubIndex((prev) => prev + (reverse ? -1 : 1));
-    }, Math.max(reverse ? 50 : subIndex === words[index].length ? 20 : 60, parseInt(Math.random() * 5)));
-
-    return () => clearTimeout(timeout);
-  }, [subIndex, index, reverse]);
+  }, [subIndex, index]);
 
   // blinker
   useEffect(() => {
-    const timeout2 = setTimeout(() => {
+    const timeout = setTimeout(() => {
       setBlink((prev) => !prev);
-    }, 2000);
-    return () => clearTimeout(timeout2);
+    }, 500);
+    return () => clearTimeout(timeout);
   }, [blink]);
 
   return (
-    <h3 className='typewriter'>{`${words[index].substring(0, subIndex)}${
-      blink ? '|' : ' '
-    }`}</h3>
+    <h3 className='typewriter'>
+      {`${words[index].substring(0, subIndex)}${blink ? '|' : ' '}`}
+    </h3>
   );
-}
+};
+
+export default TypeWriter;
